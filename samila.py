7cb594fc5e2e9131c395ca57a9074f4b8914488a
Subhashis Hansda <subhashishansda2@gmail.com>
2022-03-04 22:33:04 +0530

    static generative art

--- samila.py
+++ samila.py
@@ -12,7 +12,16 @@ import math
 import matplotlib.pyplot as plt
 from samila import GenerativeImage, Projection
 
-g = GenerativeImage()
+def f1(x, y):
+    n = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
+    return n
+
+def f2(x, y):
+    n = random.uniform(-1,1) * y**3 - math.cos(x**2) + 2*x
+    return n
+
+g = GenerativeImage(f1, f2)
+# g.generate(start=-90, step=0.01, stop=0)
 g.generate(seed = 69)
 print(g.seed)
-g.plot(color = "red", bgcolor = "black")
+g.plot(color="red", bgcolor="black", projection = Projection.RECTILINEAR)
